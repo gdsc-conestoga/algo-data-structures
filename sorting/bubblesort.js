a63056d0315ec5511805fd8325d3a9505a98db3d
ANDREYDEN <andrey2850@gmail.com>
2021-03-19 17:33:31 -0400

    Styled components

--- sorting/bubblesort.js
+++ sorting/bubblesort.js
@@ -1,23 +1,23 @@
-function bubbleSort(arr){
+function bubbleSort(arr) {
   let swapped = true;
-  for(let i = 0; i < arr.length; i++) {
-      for(let j = 0; j < arr.length; j++) {
-          if(arr[j] > arr[j + 1]) {
-              swap(arr, j, j + 1);
-          }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
       }
-      if(!swapped) break;
+    }
+    if (!swapped) break;
   }
 
   return arr;
 }
 
-function swap(arr, idx1, idx2){
+function swap(arr, idx1, idx2) {
   let temp = arr[idx1];
   arr[idx1] = arr[idx2];
   arr[idx2] = temp;
 }
 
 module.exports = {
-  bubbleSort
-}
+  bubbleSort,
+};
--- sorting/mergesort.js
+++ sorting/mergesort.js
@@ -1,37 +1,36 @@
-function merge(arr1, arr2){
-  let i = 0, // pointer over arr1
-      j = 0, // pointer over arr2
-      result = []; // to be returned
+function merge(arr1, arr2) {
+  let i = 0; // pointer over arr1
+  let j = 0; // pointer over arr2
+  let result = []; // to be returned
 
   // over both arrays
-  while(i < arr1.length && j < arr2.length){
-      if(arr1[i] < arr2[j]){
-          result.push(arr1[i]);
-          i++;
-      }
-      else{
-          result.push(arr2[j]);
-          j++;
-      }
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
   }
 
   // over arr1
-  while(i < arr1.length){
-      result.push(arr1[i]);
-      i++;
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
   }
 
   // over arr2
-  while(j < arr2.length){
-      result.push(arr2[j]);
-      j++;
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
   }
 
   return result;
 }
 
-function mergeSort(arr){
-  if(arr.length <= 1) return arr; // base case
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr; // base case
 
   let middle = Math.floor(arr.length / 2);
   let left = mergeSort(arr.slice(0, middle));
@@ -40,4 +39,4 @@ function mergeSort(arr){
   return merge(left, right);
 }
 
-module.exports = { mergeSort }
+module.exports = { mergeSort };
